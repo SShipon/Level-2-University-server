d91070eb2552387afed6dbcbfc5ce1ad6de95cb3
SShipon <nxshipon@gmail.com>
2024-05-28 16:29:36 +0100

    roures add

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 
 import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
-import globalErrorHandler from "./app/middlwares/globalErrorHandler";
-import notFound from "./app/middlwares/notFound";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import notFound from "./app/middleware/notFound";
 import router from "./app/routes";
 const app: Application = express();
+//middleware
 
 //parser
 app.use(express.json());
@@ -13,12 +14,20 @@ app.use(cors());
 //application routes
 app.use("/api/v1", router);
 
+// const test = (req:Request, res:Response)=>{
+//     const a= 10;
+//     res.send(a)
+// }
 
+// app.get('/', test)
 
+app.get('/', (req: Request, res: Response) => {
+    res.send('<h2 style="color:#119FB0 ;text-align:center; margin:15% auto; font-size:48px; font-weight: 900;">🎓This is a <span  style="color:#68037A"> University</span> Server is Running !!!  🔥🏫</h2>')
+  })
 //global error handler
 app.use(globalErrorHandler);
 
-//not found
+//not found middleware
 app.use(notFound);
 
 export default app;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,22 +1,45 @@
-import { Router } from 'express';
-import { StudentRoutes } from '../modules/student/student.route';
-import { UserRoutes } from '../modules/user/user.route';
+// import { Router } from 'express';
+// import { StudentRoutes } from '../modules/student/student.route';
+// import { UserRoutes } from '../modules/user/user.route';
 
-const router = Router();
+import { Router } from "express";
+import { UserRoutes } from "../modules/user/user.route";
+import { StudentRoutes } from "../modules/student/student.route";
 
-const moduleRoutes = [
-  //route
+// const router = Router();
+
+// const moduleRoutes = [
+//   //route
+//   {
+//     path: '/users',
+//     route: UserRoutes,
+//   },
+//   //route
+//   {
+//     path: '/students',
+//     route: StudentRoutes,
+//   },
+// ];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+// export default router;
+
+
+
+const router = Router()
+
+const moduleRoutes =[
   {
-    path: '/users',
-    route: UserRoutes,
+    path:'/users',
+    route:UserRoutes,
   },
-  //route
   {
-    path: '/students',
-    route: StudentRoutes,
+    path:'/students',
+    route:StudentRoutes,
   },
-];
+]
 
-moduleRoutes.forEach((route) => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.route))
 
 export default router;

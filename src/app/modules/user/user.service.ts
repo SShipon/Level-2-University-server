57adf718618a99c6e92ae45f9820ce73dc51c5ef
SShipon <nxshipon@gmail.com>
2024-06-10 10:39:03 +0100

    error solve

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -135,7 +135,7 @@ const createAdminIntoDB = async (password: string, payload: TFaculty) => {
   // create a user object
   const userData: Partial<TUser> = {};
 
-  //if password is not given , use deafult password
+  //if password is not given , use default password
   userData.password = password || (config.default_password as string);
 
   //set student role
